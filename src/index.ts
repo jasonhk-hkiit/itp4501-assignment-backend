213741b86ee8e9fb1c64880b60ff17c5498e0ab7
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2026-05-19 08:56:20 +0000

    Removed CDN-Cache-Control

--- src/index.ts
+++ src/index.ts
@@ -4,43 +4,42 @@ type Hand = 0 | 5;
 
 interface Hands
 {
-	left: Hand;
-	right: Hand;
+    left: Hand;
+    right: Hand;
 }
 
 interface OpponentChoices extends Hands
 {
-	guess: number;
+    guess: number;
 }
 
 const headers = new Headers(
-	{
-		"Cache-Control": "no-store",
-		"CDN-Cache-Control": "max-age=1",
-	});
+{
+    "Cache-Control": "no-store",
+});
 
 const random = new Random(Date.now());
 
 function randomHands(): Hands
 {
-	return {
-		left: random.boolean() ? 5 : 0,
-		right: random.boolean() ? 5 : 0,
-	};
+    return {
+        left: random.boolean() ? 5 : 0,
+        right: random.boolean() ? 5 : 0,
+    };
 }
 
 export default
 {
-	async fetch(request, env, ctx): Promise<Response>
-	{
-		const hands = randomHands();
-		const playerHands = randomHands();
+    async fetch(): Promise<Response>
+    {
+        const hands = randomHands();
+        const playerHands = randomHands();
 
-		const choices: OpponentChoices = {
-			...hands,
-			guess: hands.left + hands.right + playerHands.left + playerHands.right,
-		};
+        const choices: OpponentChoices = {
+            ...hands,
+            guess: hands.left + hands.right + playerHands.left + playerHands.right,
+        };
 
-		return Response.json(choices, { headers });
-	},
+        return Response.json(choices, { headers });
+    },
 } satisfies ExportedHandler<Env>;
